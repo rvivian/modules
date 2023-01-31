d602c280ee54f528dd4eb57f1f94ab4dda6875fe
Rhyss Vivian <rhyssv@gmail.com>
2023-01-30 21:48:34 -0900

    cleaning up formatting

--- examples/mysql/main.tf
+++ examples/mysql/main.tf
@@ -5,12 +5,12 @@ provider "aws" {
 module "mysql" {
   source = "github.com/rvivian/modules//data-stores/mysql"
 
-  db_prefix = var.db_prefix
+  db_prefix         = var.db_prefix
   allocated_storage = 10
-  instance_class = "db.t2.micro"
-  db_name = "example_database"
+  instance_class    = "db.t2.micro"
+  db_name           = "example_database"
 
-  db_username = "admin"
-  db_password = var.db_password
+  db_username         = "admin"
+  db_password         = var.db_password
   skip_final_snapshot = true
 }
