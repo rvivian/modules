2f858642cb647c01da9d801e5dcf9b184f168fd5
rvivian <rhyssv@gmail.com>
2023-01-31 15:19:31 -0900

    Added requirements

--- networking/alb/main.tf
+++ networking/alb/main.tf
@@ -1,3 +1,18 @@
+terraform {
+  required_version = ">= 1.0.0, < 2.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
 resource "aws_lb" "example" {
   name               = var.alb_name
   load_balancer_type = "application"
